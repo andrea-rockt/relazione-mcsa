3560a57262f00de265c0cec529008fc8674c9112
Marco Arena <arena.marco88@gmail.com>
2012-07-05 15:47:19 +0200

    aligned matlab code listings

--- src/modelling/orbit_dynamics/code/orbit_parameters.m
+++ src/modelling/orbit_dynamics/code/orbit_parameters.m
@@ -5,7 +5,9 @@ RE=6.378137e6;		%% raggio terrestre
 hs=325e3;		%% altezza dell'orbita
 a=hs+RE;           	%% semi-asse maggiore
 e=5/1000;            	%% eccentricita' dell'orbita 
-RAAN=0*deg2rad;    	%% Right Ascension of the Ascending Node
+RAAN=0*deg2rad;    	%% Right Ascension of the 
+			%% Ascending Node
 inc=96.5*deg2rad; 	%% inclinazione dell'orbita
 arg=0*deg2rad;     	%% argomento del perigeo
-Omge=0.07292115e-3; 	%% velocita' di rotazione terrestre
+Omge=0.07292115e-3; 	%% velocita' media di
+			%% rotazione terrestre
